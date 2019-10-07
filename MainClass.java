3771afde20e0dc3d1cb84a26bc6741fb5400113d
17656319795 <54891050+17656319795@users.noreply.github.com>
2019-10-07 20:40:54 +0800

    第四次作业 工厂方法部分
    第四次作业 工厂方法部分

--- MainClass.java
+++ MainClass.java
@@ -1,10 +1,12 @@
-import course.*;
-import student.*;
-import teacher.*;
-public class MainClass{
+import factory.*;
+import tv.*;
+public class MainClass {
 	public static void main(String [] args){
-		Course cs=new Course();
-		Student st=new Student();
-		Teacher te=new Teacher();
+		HaierTVFactory Factory1=new HaierTVFactory(); 		//创建海尔TV工厂
+		Tv T1=Factory1.produceTv();
+		HisenseTVFactory Factory2=new HisenseTVFactory();	//创建海信TV工厂
+		Tv T2=Factory2.produceTv();
+		T1.play();
+		T2.play();
 	}
 }
