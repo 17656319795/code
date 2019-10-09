7958eb54e3f7cde26255bc18a0834afde38bed59
17656319795 <54891050+17656319795@users.noreply.github.com>
2019-10-09 09:27:34 +0800

    第四次作业 工厂方法部分
    第四次作业 工厂方法部分

--- Main.java
+++ Main.java
@@ -1,34 +1,13 @@
-package javaTest;
-
-import java.util.Scanner;
+import factory.*;
+import tv.*;
+import utility.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner in=new Scanner(System.in);
-		int n=in.nextInt();
-		for(int j=0;j<n;j++)
-		{
-			String str=in.nextLine();
-			System.out.println(str);
-			str=str.replace("I", "you");
-			str=str.replace("me", "you");
-			str=str.replace("?", "!");
-			str=str.replace(" ", "&");
-			String str2[]=str.split("&");
-			int i=0;
-			String str3="";
-			for(String a:str2)
-			{
-				if(str2[i].contains(" ")!=true)
-				str3=str3+" "+str2[i++];
-			}
-			str3=str3.replace("can you", "I can");
-			str3=str3.replace("could you", "I could");
-//			str2[i-1]=str2[i-1].replace(" ","");
-			System.out.print("AI:"+str3);
-			System.out.print('\n');
-		}
+	public static void main(String [] args){
+		TV tv;
+		TVFactory factory;
+		factory=(TVFactory)XMLUtil.getBean();
+		tv=factory.produceTV();
+		tv.play();
 	}
-
 }
