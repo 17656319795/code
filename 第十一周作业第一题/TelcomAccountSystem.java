499e679cea287e2e38277f43e89121e543bb3faf
17656319795 <54891050+17656319795@users.noreply.github.com>
2019-11-18 19:40:38 +0800

    第十一周作业第一题
    第十一周作业第一题

---
+++ 第十一周作业第一题/TelcomAccountSystem.java
@@ -0,0 +1,11 @@
+package 第十一周作业;
+
+public class TelcomAccountSystem {
+
+	public static void main(String[] args) {
+		TelcomUser telcomUser=new TelcomUser("17656319795");
+		telcomUser.generateCommucationRecord();
+		telcomUser.printDetails();
+	}
+
+}
