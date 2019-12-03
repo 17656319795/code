17598a058641949bf2571991d9d3363910d42279
17656319795 <54891050+17656319795@users.noreply.github.com>
2019-12-03 13:29:06 +0800

    用状态7的方法处理frequentRenterPoints
    用状态7的方法处理frequentRenterPoints

---
+++ 代码重构(状态8)/Main.java
@@ -0,0 +1,12 @@
+package ´úÂëÖØ¹¹;
+
+public class Main {
+		public static void main(String[] args) {
+			Customer person1=new Customer("HuangZhichao");
+			int price=20,days=5;
+			Movie movie1=new Movie("Harry potter",price);
+			Rental rental1=new Rental(movie1,days);
+			person1.addRental(rental1);
+			System.out.print(person1. statement());
+		}
+}
